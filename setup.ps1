fb2ed863e522c90c85e100d57f7253f1bd68a0cb
BeeCos1 <159466752+BeeCos1@users.noreply.github.com>
2026-04-09 10:14:27 +0500

    setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,20 @@
 Write-Host "Скачивание Scanner PRO..." -ForegroundColor Cyan
 
-# 1. Создаем папку на компе (скрытая установка)
+# 1. Создаем папку на компе
 $InstallDir = "C:\IT_Tools\Scanner"
-New-Item -Path $InstallDir -ItemType Directory -Force | Out-Null
+if (!(Test-Path $InstallDir)) {
+    New-Item -Path $InstallDir -ItemType Directory -Force | Out-Null
+}
 
-# 2. Скачиваем твой pscan.exe (!!! ЗАМЕНИ ССЫЛКУ НА СВОЮ !!!)
+# 2. Скачиваем твой pscan.exe (ССЫЛКА ИСПРАВЛЕНА ПОД ТВОЙ РЕПО)
 $ExePath = "$InstallDir\pscan.exe"
-Invoke-WebRequest -Uri "https://github.com/ТВОЙ_ЛОГИН/ТВОЙ_РЕПОЗИТОРИЙ/raw/main/pscan.exe" -OutFile $ExePath
+$Url = "https://github.com/BeeCos1/PrinterScanner/raw/main/pscan.exe"
+Invoke-WebRequest -Uri $Url -OutFile $ExePath
 
-# 3. Создаем глобальную команду pscan
+# 3. Создаем глобальную команду pscan (Добавили -Force для обхода защиты)
 $BatPath = "C:\Windows\pscan.bat"
-"@echo off`nstart `"`" `"$ExePath`"" | Out-File -FilePath $BatPath -Encoding ascii
+$BatContent = "@echo off`nstart `"`" `"$ExePath`""
+Set-Content -Path $BatPath -Value $BatContent -Force
 
 Write-Host "Готово! Запускаю сканер..." -ForegroundColor Green
 Start-Process $ExePath
